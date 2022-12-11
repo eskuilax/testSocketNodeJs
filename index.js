d03a49346bdaaaf8b4f36ff79b0d06fd061ab7a3
francisco parada <fram@hotmail.cl>
2022-12-11 00:37:35 -0300

    port

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const SocketIO = require('socket.io');
-
+const io = SocketIO(server);
 
 //setting
-app.set('port', process.env.PORT || 3000);
+
+app.set('port', process.env.PORT);
 
 
 //static file
@@ -16,7 +17,7 @@ const server = app.listen(app.get('port'), ()=>{
     console.log("server on port ",app.get('port'));
 });
 
-const io = SocketIO(server);
+
 
 //websocket
 io.on('connection', (socket)=>{
